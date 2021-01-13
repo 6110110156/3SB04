c147e83b1035e76ce73dfbb65d61e522b37dad79
6110110156 <6110110156@psu.ac.th>
2021-01-14 04:08:19 +0700

    hi4

--- python_intro.py
+++ python_intro.py
@@ -1,9 +1,4 @@
 def hi(name):
-    if name == 'Ola':
-        print('Hi Ola!')
-    elif name == 'Sonja':
-        print('Hi Sonja!')
-    else:
-        print('Hi anonymous!')
+    print('Hi ' + name + '!')
 
-hi("Ola")
+hi("Rachel")
