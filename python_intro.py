df157037958334edf8e8036d10a50b5e57a39549
6110110156 <6110110156@psu.ac.th>
2021-01-14 04:03:11 +0700

    57

--- python_intro.py
+++ python_intro.py
@@ -1,7 +1,13 @@
-name = 'Sonja'
-if name == 'Ola':
-    print('Hey Ola!')
-elif name == 'Sonja':
-    print('Hey Sonja!')
+volume = 57
+if volume < 20:
+    print("It's kinda quiet.")
+elif 20 <= volume < 40:
+    print("It's nice for background music")
+elif 40 <= volume < 60:
+    print("Perfect, I can hear all the details")
+elif 60 <= volume < 80:
+    print("Nice for parties")
+elif 80 <= volume < 100:
+    print("A bit loud!")
 else:
-    print('Hey anonymous!')
+    print("My ears are hurting! :(")
